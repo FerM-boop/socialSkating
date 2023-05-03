6f9761992fef7fa3b6f2f3eff220cc77a78e7a33
unknown <fermejorado1@gmail.com>
2023-05-02 21:24:13 -0600

    Trying to style first page, app works on web but not on phone

--- app/index.js
+++ app/index.js
@@ -2,16 +2,18 @@ import { ImageBackground, StyleSheet, Text, View } from "react-native";
 import { Link } from "expo-router";
 import { Button } from "react-native-web";
 
+const gifSource = {uri: '../assets/skate_gif.gif'}
+
 export default function Page() {
   return (
     <View style={styles.container}>
-      <ImageBackground source="../assets/skate_gif.gif" resizeMode="cover" style={styles.image}>
+      <ImageBackground source={gifSource} resizeMode="cover" style={styles.image}>
         <View style={styles.main}>
           <Text style={styles.title}>Social Skating</Text>
           <Text style={styles.subtitle}>A social media by skaters for skaters.</Text>
           <Link href="/auth/landing">
             <Button
-              title="Get started"
+              title= <Text>Get started</Text>
             />
           </Link>
         </View>

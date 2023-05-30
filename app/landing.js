7ca89c50fb06357348789cb1d340d755abc5a846
unknown <fermejorado1@gmail.com>
2023-05-30 17:37:02 -0600

    Changed the landing page export from a class back to a function with useState and useEffect to check for loaded and loggedin status

--- app/landing.js
+++ app/landing.js
@@ -1,5 +1,5 @@
 import { View, Text, Pressable, StyleSheet } from 'react-native'
-import React, { useState, useEffect, Component } from 'react'
+import React, { useState, useEffect } from 'react'
 
 
 import { initializeApp } from "firebase/app";

68a155bcc73a11aa44fece238e7c856ea35e1c9e
unknown <fermejorado1@gmail.com>
2023-06-07 20:59:51 -0600

    Map now loads in on users location

--- app/map.js
+++ app/map.js
@@ -1,12 +1,76 @@
-import { View } from "react-native";
-import { Link, Stack } from "expo-router";
+import React, { useState, useEffect } from 'react';
+import MapView, {Marker} from 'react-native-maps';
+import { Pressable, StyleSheet, View, Text, Dimensions } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function Map() {
+  const [mapRegion, setMapRegion] = useState({
+    latitude: 21.307585,
+    longitude: -157.850665,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+  const userLocation = async () => {
+    let {status} = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      setErrorMsg('Permission to access location was denied');
+    }
+    
+    let location = await Location.getCurrentPositionAsync({accuracy: Location.Accuracy.Highest, maximumAge:10000});
+    console.log(location.coords.latitude, location.coords.longitude)
+    setMapRegion({
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    }); 
+  }
+
+
+  useEffect(() => {
+    userLocation();
+  }, []);
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Stack.Screen options={{ }} />
-      {/* Use the `Link` component to enable optimized client-side routing. */}
-      <Link href="/details">Map</Link>
+    <View style={styles.container}>
+      <Pressable
+        style={styles.button}
+        onPress={() => {
+          userLocation()
+        }}>
+        <Text>
+        Get Location
+        </Text>
+      </Pressable>
+      <MapView style={styles.map} 
+        region={mapRegion}
+      >
+      <Marker coordinate={mapRegion} title='Marker'/>
+      </MapView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 20,
+    elevation: 3,
+    backgroundColor: '#0a0a0a',
+    height: 60,
+    width: 160,
+    opacity: 0.7,
+    margin: 5
+  },
+});
